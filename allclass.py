5e37c6bb7b3f6d70a1171ade82f478ba7237cf1d
balaji <mbalaji211@gmail.com>
2022-09-27 11:42:42 +0530

    online class

---
+++ allclass.py
@@ -0,0 +1,32 @@
+###DAY - 2###
+
+###FIRST PROGRAM
+
+print('Hi i am balaji')
+print("Hi i am balaji")
+print('''Hi i am balaji''')
+
+###GET INPUT FROM USER
+
+name=input('What is your name = ')
+age=int(input('What is your age = '))
+
+print('My name is ',name,'I am ',age,'years old')
+
+###ACCESSING THE INDEX FROM GIVEN STRING
+
+name = 'Balaji'
+print(name)
+print(name[0])
+print(name[-1])
+print(name[2:5])
+print(name[::2])
+
+###ACCESSING THE INDEX FROM GIVEN STRING
+
+name = 'muthuramalingam'
+print(name)
+print(name[2])
+print(name[-7])
+print(name[1:5])#slicing operator
+print(name[-1:-5:-2])#step operator
