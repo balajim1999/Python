9a16cdd6360656df211c9cf6c8868296aefbcacb
balaji <mbalaji211@gmail.com>
2022-09-27 15:56:00 +0530

    update

--- allclass.py
+++ allclass.py
@@ -30,3 +30,77 @@ print(name[2])
 print(name[-7])
 print(name[1:5])#slicing operator
 print(name[-1:-5:-2])#step operator
+
+###DAY - 3###
+
+#Type casting
+#float to int
+price = 108.23      #float value
+print(type(price))
+change_price = int(price)  #int value
+print(type(change_price))
+
+
+#Type casting
+#boolean to int
+
+today_class = True      #boolean
+print(type(today_class))
+print(today_class)
+change = int(today_class)
+print(type(change))
+
+#Type casting
+#string to int
+
+doorno = "1234" # string
+change = int(doorno)
+print(change)
+print(type(change))
+
+print(change)
+
+#Type casting
+#to float
+price = 100
+print(float(price))
+
+print(float(False))
+print(float(True))
+print(float("20"))
+print(float("20.5"))
+
+#Operators
+#Relational Operators
+name1 = 'balaji'
+name2 = 'ram'
+print(name1 == name2)
+print(name1 > name2)
+print(name1 < name2)
+
+#Relational Operators #Asky value
+name1 = 'balaji'
+name2 = 'Ram'
+print(name1 == name2)
+print(name1 > name2)
+print(name1 < name2)
+
+#Operator Chaining
+no1, no2, no3 = 10, 15, 20
+print(no1<no2<no3)
+print(no1>no2>no3)
+print(no1!=no2!=no3)
+print(no1>no2<no3) 
+
+
+#Arithmetic Operators
+print(2+3)
+print(2-3)
+print(2*3)
+print(2/3)
+print(2//3)
+print(20//3)#floor division
+print(20/3)
+print(20%3)
+print(2**3) 
+
