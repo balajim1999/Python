3a276bb0685cf58a5054a94a5d5329c13571c021
balaji <mbalaji211@gmail.com>
2022-10-04 10:55:11 +0530

    auto whatsapp msh

---
+++ whatsapp/msg.py
@@ -0,0 +1,6 @@
+#Send automated whatsapp message 
+#intsall #pywhatkit module
+#install #sudo apt-get install python3-tk python3-dev
+
+import pywhatkit
+pywhatkit.sendwhatmsg("+918220XXXXXX","Hi this is balaji",10,50)
